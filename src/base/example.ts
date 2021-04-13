ab6eeab011c40afac6b4219a2b3663bff0438f1d
wsafight <984292420@qq.com>
2021-04-13 22:37:55 +0800

    feat: add template

--- src/base/example.ts
+++ src/base/example.ts
@@ -1,19 +1,18 @@
 import SuperExpressive from 'super-expressive';
 
 const regex = SuperExpressive()
-  .allowMultipleMatches
-  .lineByLine
-  .startOfInput
-  .optional.string('0x')
-  .capture
-  .exactly(4).anyOf
-  .range('A', 'F')
-  .range('a', 'f')
-  .range('0', '9')
-  .end()
-  .end()
-  .endOfInput
-  .toRegex();
+    .allowMultipleMatches
+    .lineByLine
+    .startOfInput
+    .optional.string('0x')
+    .capture
+    .exactly(4).anyOf
+    .range('A', 'F')
+    .range('a', 'f')
+    .range('0', '9')
+    .end()
+    .end()
+    .endOfInput
+    .toRegex();
 
 export const resultCode = regex.source
-console.log(resultCode)
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {resultCode} from './base/example'
+
+const obj = {
+    demo1: resultCode
+}
+
+console.log('<<<<<电话正则<<<<<')
+console.log(obj)
+console.log('>>>>>电话正则>>>>>')
